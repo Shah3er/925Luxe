bfe6b5a1c0380df07c36db602896896588da61b3
Shah3er <78930700+Shah3er@users.noreply.github.com>
2025-05-16 17:21:36 -0400

    Update server.py

--- server.py
+++ server.py
@@ -55,4 +55,6 @@ def create_checkout_session():
         return jsonify(error=str(e)), 403
 
 if __name__ == '__main__':
-    app.run(port=4242)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
